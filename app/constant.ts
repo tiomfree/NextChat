7552873489288aab5b3ccb6528781be677093595
tiomanfree <149781951+tiomfree@users.noreply.github.com>
2025-06-01 16:42:04 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -546,6 +546,8 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
+  "gemini-2.5-flash-preview-04-17",
+  "gemini-2.5-flash-preview-05-20"
 ];
 
 const anthropicModels = [
